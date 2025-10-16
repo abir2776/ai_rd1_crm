4a76e72451ce83560e8f8b6fcc249df274e674dc
abir2776 <osmangoni255@gmail.com>
2025-10-16 16:18:33 +0600

    changed db config sqlite to postgres

--- organizations/admin.py
+++ organizations/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Organization, Platform, OrganizationPlatform, OrganizationUser
+from .models import (
+    Organization,
+    Platform,
+    OrganizationPlatform,
+    OrganizationUser,
+    OrganizationUserInvitation,
+)
 
 # Register your models here.
 admin.site.register(Organization)
 admin.site.register(Platform)
 admin.site.register(OrganizationPlatform)
 admin.site.register(OrganizationUser)
+admin.site.register(OrganizationUserInvitation)
