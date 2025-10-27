943c959c865b91c6e62c079286f57a0bff54a5b6
abir2776 <osmangoni255@gmail.com>
2025-10-27 16:06:08 +0600

    Fixed issue in phone number api serializers

--- phone_number/rest/serializers/phone_numbers.py
+++ phone_number/rest/serializers/phone_numbers.py
@@ -1,36 +1,35 @@
 from rest_framework import serializers
-
 from phone_number.models import (
-    RegulatoryAddress,
     RegulatoryBundle,
-    SupportingDocument,
+    RegulatoryAddress,
     TwilioPhoneNumber,
+    SupportingDocument,
 )
 
 
 class RegulatoryBundleSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = RegulatoryBundle
         fields = ("__all__",)
         read_only_fields = ("__all__",)
 
 
 class RegulatoryAddressSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = RegulatoryAddress
         fields = ("__all__",)
         read_only_fields = ("__all__",)
 
 
 class PhoneNumberSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = TwilioPhoneNumber
         fields = ("__all__",)
         read_only_fields = ("__all__",)
 
 
 class SupportingDocumentSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = SupportingDocument
         fields = ("__all__",)
         read_only_fields = ("__all__",)
