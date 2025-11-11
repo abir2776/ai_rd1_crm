70f2658f75201ccf792b97ac07972beb44134703
abir2776 <osmangoni255@gmail.com>
2025-11-11 11:52:30 +0600

    removed create method from interview save endpoint

--- interview/rest/serializers/interview.py
+++ interview/rest/serializers/interview.py
@@ -8,11 +8,3 @@ class InterviewTakenSerializer(serializers.ModelSerializer):
         model = InterviewTaken
         fields = "__all__"
         read_only_fields = []
-
-    def create(self, validated_data):
-        user = self.context["request"].user
-        organization = user.get_organization()
-        interview = InterviewTaken.objects.create(
-            **validated_data, organization=organization
-        )
-        return interview
