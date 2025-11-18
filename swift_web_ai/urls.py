4c0bfd93aded2778893cc8ea0d775d18124c561c
abir2776 <osmangoni255@gmail.com>
2025-11-18 12:18:47 +0600

    Fixed audio file not found issue

--- swift_web_ai/urls.py
+++ swift_web_ai/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_yasg import openapi
@@ -77,3 +79,4 @@ urlpatterns = [
     path("api/v1/organizations/", include("organizations.rest.urls")),
     path("api/v1/subscription/", include("subscription.rest.urls")),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
