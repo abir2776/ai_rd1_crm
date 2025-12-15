a4e6f16535bdcff608eb7bd40f11f66edc483c14
abir2776 <osmangoni255@gmail.com>
2025-12-15 16:56:51 +0600

    fixed 400 error in email send

--- common/tasks.py
+++ common/tasks.py
@@ -4,15 +4,25 @@ from django.template.loader import render_to_string
 
 
 @shared_task
-def send_email_task(subject, recipient, template_name, context):
+def send_email_task(
+    subject,
+    recipient,
+    template_name,
+    context,
+    customer_email="osmangoni00255@gmail.com",
+):
     html_content = render_to_string(template_name, context)
-    from_email = f"{'osmangoni00255@gmail.com'} via AIRD1 <no-reply@rd1.co.uk>"
+    if customer_email:
+        from_email = f'"{customer_email} via AIRD1" <no-reply@rd1.co.uk>'
+    else:
+        from_email = "AIRD1 <no-reply@rd1.co.uk>"
 
     email = EmailMultiAlternatives(
         subject=subject,
         body="Please view this email in an HTML-compatible email client.",
         from_email=from_email,
         to=[recipient],
+        reply_to=[customer_email] if customer_email else None,
     )
     email.attach_alternative(html_content, "text/html")
     email.send()
