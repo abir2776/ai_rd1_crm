071babcae70607aed99ad661c57aa0de7e2dbd60
abir2776 <osmangoni255@gmail.com>
2025-12-17 13:58:18 +0600

    added endpoint for placement status and payment type

--- awr_compliance/rest/urls/__init__.py
+++ awr_compliance/rest/urls/__init__.py
@@ -2,4 +2,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("config/", include("awr_compliance.rest.urls.config")),
+    path("list/", include("awr_compliance.rest.urls.list")),
 ]
