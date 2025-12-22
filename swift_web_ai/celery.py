0850784a3fb472f4014151b5e6acd2c77ed42a38
osmangoni255 <osmangoni255@gmail.com>
2025-12-22 21:22:08 +0600

    uncomment sms task for testing

--- swift_web_ai/celery.py
+++ swift_web_ai/celery.py
@@ -34,14 +34,14 @@ app.conf.beat_schedule = {
     #     "task": "interview.tasks.ai_phone.initiate_all_interview",
     #     "schedule": crontab(minute="*/3"),
     # },
-    "format-cvs-every-3-minutes": {
-        "task": "cv_formatter.tasks.initiate_all_cv_formatting",
-        "schedule": crontab(minute="*/3"),
-    },
-    # "run-initiate-all-sms-interview-every-3-min": {
-    #     "task": "interview.tasks.ai_sms.initiate_all_sms_interviews",
+    # "format-cvs-every-3-minutes": {
+    #     "task": "cv_formatter.tasks.initiate_all_cv_formatting",
     #     "schedule": crontab(minute="*/3"),
     # },
+    "run-initiate-all-sms-interview-every-3-min": {
+        "task": "interview.tasks.ai_sms.initiate_all_sms_interviews",
+        "schedule": crontab(minute="*/3"),
+    },
     # "initiate-whatsapp-interviews": {
     #     "task": "interview.tasks.ai_whatsapp.initiate_all_whatsapp_interviews",
     #     "schedule": crontab(minute="*/3"),
