0b90ba803d3f2317d31916d9b9feb23199557fe1
abir2776 <osmangoni255@gmail.com>
2025-12-29 12:21:55 +0600

    added new model in admin

--- ai_lead_generation/admin.py
+++ ai_lead_generation/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import LeadGenerationConfig
+from .models import LeadGenerationConfig, MarketingAutomationConfig
 
 admin.site.register(LeadGenerationConfig)
+admin.site.register(MarketingAutomationConfig)
