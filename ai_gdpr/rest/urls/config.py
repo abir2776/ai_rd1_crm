a07044f1bdd2bf4395e9edcec13e63634525b6b6
abir2776 <osmangoni255@gmail.com>
2025-12-31 12:00:00 +0600

    added url for gdpr config detais

--- ai_gdpr/rest/urls/config.py
+++ ai_gdpr/rest/urls/config.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from ai_gdpr.rest.views.config import GDPREmailConfigListCreateView
+from ai_gdpr.rest.views.config import (
+    GDPREmailConfigDetailView,
+    GDPREmailConfigListCreateView,
+)
 
 urlpatterns = [
     path("", GDPREmailConfigListCreateView.as_view(), name="gdpr-email-config"),
+    path(
+        "details", GDPREmailConfigDetailView.as_view(), name="gdpr-email-config-details"
+    ),
 ]
