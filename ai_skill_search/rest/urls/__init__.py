b84a7bd3a999750cb1f19787d3167bcf8030cd14
abir2776 <osmangoni255@gmail.com>
2026-01-05 12:19:41 +0600

    added candidate status endpoint

--- ai_skill_search/rest/urls/__init__.py
+++ ai_skill_search/rest/urls/__init__.py
@@ -3,4 +3,5 @@ from django.urls import include, path
 urlpatterns = [
     path("config/", include("ai_skill_search.rest.urls.config")),
     path("reports/", include("ai_skill_search.rest.urls.reports")),
+    path("list/", include("ai_skill_search.rest.urls.list")),
 ]
