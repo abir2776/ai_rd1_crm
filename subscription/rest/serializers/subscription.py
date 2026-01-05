ec2641f378e85baabf5a8c16bc70bc096053b72c
abir2776 <osmangoni255@gmail.com>
2026-01-05 12:47:15 +0600

    fixed subscription serializer

--- subscription/rest/serializers/subscription.py
+++ subscription/rest/serializers/subscription.py
@@ -17,14 +17,14 @@ class SubscriptionSerializer(serializers.ModelSerializer):
             "auto_renew",
             "plan_feature_uid",
         ]
-        read_only_fields = ["uid"]
+        read_only_fields = ["uid", "organization", "plan_feature", "available_limit"]
 
     def create(self, validated_data):
         plan_feature_uid = validated_data.pop("plan_feature_uid")
         user = self.context["request"].user
         organization = user.get_organization()
         plan_feature = PlanFeature.objects.filter(uid=plan_feature_uid).first()
-        if plan_feature == None:
+        if not plan_feature:
             raise serializers.ValidationError(
                 "Plan feature not found with this given uid."
             )
