707bccf27c11aaac277e1202303219133f485dfe
abir2776 <osmangoni255@gmail.com>
2026-01-14 18:07:28 +0600

    added stepen number for testing

--- whatsapp_campaign/tasks.py
+++ whatsapp_campaign/tasks.py
@@ -107,7 +107,7 @@ def fetch_contacts_from_jobadder(
                             "contact_id": contact.get("contactId"),
                             "name": f"{contact.get('firstName', '')} {contact.get('lastName', '')}".strip(),
                             "email": contact.get("email", ""),
-                            "phone": "+8801815553036",
+                            "phone": "+447872603687",
                             "company": contact.get("company", ""),
                         }
                         contacts.append(contact_data)
