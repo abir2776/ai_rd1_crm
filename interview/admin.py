0ae094472d0c8db59fe029865cdfdf0c98ec0863
abir2776 <osmangoni255@gmail.com>
2026-01-15 17:25:03 +0600

    added admin file for call request

--- interview/admin.py
+++ interview/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from .models import (
     AIMessageConfig,
     AIPhoneCallConfig,
+    CallRequest,
     InterviewCallConversation,
     InterviewMessageConversation,
     InterviewTaken,
+    MeetingBooking,
     PrimaryQuestion,
     QuestionConfigConnection,
     QuestionMessageConfigConnection,
@@ -19,3 +21,5 @@ admin.site.register(QuestionConfigConnection)
 admin.site.register(AIMessageConfig)
 admin.site.register(InterviewMessageConversation)
 admin.site.register(QuestionMessageConfigConnection)
+admin.site.register(CallRequest)
+admin.site.register(MeetingBooking)
