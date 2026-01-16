130005657b576b789f9611d923efd2e4d11b65a9
abir2776 <osmangoni255@gmail.com>
2026-01-16 11:50:20 +0600

    added call limit for client call request

--- interview/rest/views/client_call_test.py
+++ interview/rest/views/client_call_test.py
@@ -1,6 +1,9 @@
-# calls/views.py
+from datetime import timedelta
+
+from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -13,29 +16,57 @@ from interview.rest.serializers.client_call_test import (
 )
 from interview.tasks.ai_phone import initiate_call
 from interview.throttles import CallRequestIPThrottle
-from interview.utils import local_to_utc
 
 
 class CallRequestCreateView(APIView):
     permission_classes = [AllowAny]
-    # throttle_classes = [CallRequestIPThrottle]
+    throttle_classes = [CallRequestIPThrottle]
 
     def post(self, request):
         serializer = CallRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         data = serializer.validated_data
+        phone = data["phone"]
+
+        twelve_hours_ago = timezone.now() - timedelta(hours=12)
+
+        call_count = CallRequest.objects.filter(
+            phone=phone,
+            created_at__gte=twelve_hours_ago,
+        ).count()
+        schedule_call = CallRequest.objects.filter(phone=phone,call_type="SCHEDULE",is_called=False)
+        if schedule_call.exists:
+            raise ValidationError(
+                {
+                    "detail": (
+                        "You already have incomplete schedule call."
+                        "Please try again later after completing already scheduled call."
+                    )
+                }
+            )
+        
+        if call_count >= 2:
+            raise ValidationError(
+                {
+                    "detail": (
+                        "You can place a maximum of 2 call requests "
+                        "within 12 hours. Please try again later."
+                    )
+                }
+            )
+
         scheduled_at = None
         if data["call_type"] == "SCHEDULE":
             scheduled_at = data["scheduled_at"]
 
         call_request = CallRequest.objects.create(
             name=data["name"],
-            phone=data["phone"],
+            phone=phone,
             call_type=data["call_type"],
             scheduled_at=scheduled_at,
-            company_name = data["company_name"],
-            company_size= data["company_size"]
+            company_name=data["company_name"],
+            company_size=data["company_size"],
         )
 
         if call_request.call_type == CallRequest.CALL_NOW:
@@ -59,5 +90,5 @@ class MeetingBookingAPIView(ListCreateAPIView):
     serializer_class = MeetingBookingSerializer
     permission_classes = [AllowAny]
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ["status","scheduled_at"]
+    filterset_fields = ["status", "scheduled_at"]
     queryset = MeetingBooking.objects.filter()
